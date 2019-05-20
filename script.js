44aef43326ced2091d692a8fa4905d4d4a7cfc9b
Pasha <50833343+pasha-d1207@users.noreply.github.com>
2019-05-20 17:06:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+var $startButton = document.querySelector('#start')
+var $endButton = document.querySelector('#end')
+var $reset = document.querySelector('#reset')
+var $time = document.querySelector('#time')
+var $input = document.querySelector('#input')
+
+
+$startButton.addEventListener('click', startGame)
+$endButton.addEventListener('click', endGame)
+$reset.addEventListener('click', reset)
+$input.addEventListener('input', setGetTime)
+
+var $isGameStarted = "stop"
+
+
+function startGame(){
+    $isGameStarted = "start"
+    $startButton.setAttribute('disabled',true)
+    interval()
+}
+function endGame(){
+    $isGameStarted = "stop"
+    $startButton.removeAttribute('disabled',true)
+    interval()
+}  
+function reset(){
+    $isGameStarted = "reset"
+    $startButton.removeAttribute('disabled',true)
+    interval()
+}    
+function interval(){
+var interval = setInterval(function (){
+    if($isGameStarted === "start"){
+        var time = parseFloat($time.textContent)    
+        $time.textContent = (time - 0.1).toFixed(2) 
+    }
+    else if($isGameStarted === "stop"){
+        clearInterval(interval)
+    }
+    else if ($isGameStarted === "reset"){
+        $time.textContent = '0.0'
+        clearInterval(interval)
+    }
+    if (time <= 0){
+        $time.textContent = '0.0'
+        clearInterval(interval)
+        $startButton.removeAttribute('disabled',true)
+    }
+    }, 100)
+}
+function setGetTime(){
+    var time = +$input.value
+    $time.textContent = time.toFixed(1)    
+}
